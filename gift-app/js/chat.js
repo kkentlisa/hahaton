8a0620af7b75d4623eb8227ca4a3a85008154931
Серафима <simapono.4@gmail.com>
2026-07-03 14:36:11 +0700

    подключить бд

---
+++ gift-app/js/chat.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAe4ZXtxKH119CrCWBXM7ZjXDyiH5n3tjU",
+    authDomain: "birthday-wishes-hits.firebaseapp.com",
+    projectId: "birthday-wishes-hits",
+    storageBucket: "birthday-wishes-hits.appspot.com",
+    messagingSenderId: "650338158827",
+    appId: "1:650338158827:web:0bd1caba97a65a2de1d6ae"
+};
+
